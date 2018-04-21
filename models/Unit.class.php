0766acc1074bcf8811f86f99ea7a0281c5922ed2
alphonse-sadish Aroul <alphonse-sadish@outlook.fr>
2018-04-21 21:37:42 +0200

    Update Unit.class.php

--- models/Unit.class.php
+++ models/Unit.class.php
@@ -4,6 +4,8 @@ class Unit extends BaseSQL {
 
 	protected $id;
 	protected $name;
+	protected $deactivated;
+	protected $created_at;
 
 	public function __construct() {
 		parent::__construct();
@@ -17,10 +19,20 @@ class Unit extends BaseSQL {
 		return $this->name;
 	}
 
-	public function setName( $name ) {
-		$this->name = strtolower(trim($name));
+	public function setName($name) {
+		$this->name = strtolower(trim(addslashes($name)));
 	}
-
-
+    public function getCreated(){
+        return $this->created_at;
+    }
+    public function setCreated(){
+        $this->created_at = date("Ymd");
+    }
+    public function getDeactivated(){
+        return $this->deactivated;
+    }
+    public function setDeactivated(){
+        $this->deactivated = date("Ymd");
+    }
 
 }
