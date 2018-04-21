7e7ac3c44414342c5cf5ceecd46f4428cd9dbdc6
alphonse-sadish Aroul <alphonse-sadish@outlook.fr>
2018-04-21 21:36:50 +0200

    Update Recipe.class.php

--- models/Recipe.class.php
+++ models/Recipe.class.php
@@ -3,7 +3,6 @@
 class Recipe extends BaseSQL {
 
     protected $id;
-    protected $name;
     protected $preparation_time;
     protected $bake_time;
     protected $nb_guests;
@@ -12,7 +11,7 @@ class Recipe extends BaseSQL {
     protected $difficulty;
     protected $author_id;
     protected $created_at;
-    protected $deleted_at;
+    protected $deactivated;
 
     public function __construct()
     {
@@ -31,7 +30,7 @@ class Recipe extends BaseSQL {
 
     public function setPreparationTime($preparation_time)
     {
-        $this->preparation_time = $preparation_time;
+        $this->preparation_time = trim(strtolower(addslashes($preparation_time)));
     }
 
     public function getBakeTime()
@@ -41,7 +40,7 @@ class Recipe extends BaseSQL {
 
     public function setBakeTime($bake_time)
     {
-        $this->bake_time = $bake_time;
+        $this->bake_time = trim(addslashes($bake_time));
     }
 
     public function getNbGuests()
@@ -51,7 +50,7 @@ class Recipe extends BaseSQL {
 
     public function setNbGuests($nb_guests)
     {
-        $this->nb_guests = $nb_guests;
+        $this->nb_guests = trim(addslashes($nb_guests));
     }
 
     public function getImgUrl()
@@ -61,7 +60,7 @@ class Recipe extends BaseSQL {
 
     public function setImgUrl($img_url)
     {
-        $this->img_url = $img_url;
+        $this->img_url = trim($img_url);
     }
 
     public function getPlateType()
@@ -71,7 +70,7 @@ class Recipe extends BaseSQL {
 
     public function setPlateType($plate_type)
     {
-        $this->plate_type = $plate_type;
+        $this->plate_type = trim(addslashes(strtolower($plate_type)));
     }
 
     public function getDifficulty()
@@ -81,7 +80,7 @@ class Recipe extends BaseSQL {
 
     public function setDifficulty($difficulty)
     {
-        $this->difficulty = $difficulty;
+        $this->difficulty = trim(addslashes(strtolower($difficulty)));
     }
 
     public function getAuthorId()
@@ -91,65 +90,19 @@ class Recipe extends BaseSQL {
 
     public function setAuthorId($author_id)
     {
-        $this->author_id = $author_id;
+        $this->author_id = trim($author_id);
     }
 
-    public function getCreatedAt()
-    {
+    public function getCreated(){
         return $this->created_at;
     }
-
-    public function setCreatedAt($created_at)
-    {
-        $this->created_at = $created_at;
+    public function setCreated(){
+        $this->created_at = date("Ymd");
     }
-
-    public function getDeletedAt()
-    {
-        return $this->deleted_at;
+    public function getDeactivated(){
+        return $this->deactivated;
     }
-
-    public function setDeletedAt($deleted_at)
-    {
-        $this->deleted_at = $deleted_at;
+    public function setDeactivated(){
+        $this->deactivated = date("Ymd");
     }
-
-	public function getName() {
-		return $this->name;
-	}
-
-	public function setName( $name ) {
-		$this->name = $name;
-	}
-
-
-
-	public function findById($recipeId = 1) {
-    	$sql = <<<SQL
- 				SELECT r.name, r.bake_time, r.preparation_time, r.difficulty, r.img_url, r.nb_guests, r.plate_type 
-				FROM recipe r
-				WHERE r.id = :id
-SQL;
-    	$stmt = $this->pdo->prepare($sql);
-    	$stmt->execute(['id' => $recipeId]);
-    	return $stmt->fetch();
-    }
-
-	public function findRecipeUnit($recipeId) {
-		$sql = <<<SQL
-				SELECT ru.quantity, i.name as ingredient_name, i.img_url, IF(ru.quantity > 1, u.plural_name, u.name ) as unit_name
-				FROM recipe_unit ru
-			    JOIN ingredient i ON ru.ingredient_id = i.id
-				JOIN unit u ON ru.unit_id = u.id
-				WHERE ru.recipe_id = :id
-SQL;
-		$stmt = $this->pdo->prepare($sql);
-		$stmt->execute(['id' => $recipeId]);
-		return $stmt->fetchAll();
-
-
-
-    }
-    
- 
 }
