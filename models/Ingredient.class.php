9337a4169ab30b8eb4996d95842b328ce5ea43f6
alphonse-sadish Aroul <alphonse-sadish@outlook.fr>
2018-04-21 21:36:21 +0200

    Update Ingredient.class.php

--- models/Ingredient.class.php
+++ models/Ingredient.class.php
@@ -6,6 +6,8 @@ class Ingredient extends BaseSQL {
 	protected $id;
 	protected $name;
 	protected $imgUrl;
+	protected $created_at;
+	protected $deactivated;
 
 	public function __construct() {
 		parent::__construct();
@@ -19,16 +21,30 @@ class Ingredient extends BaseSQL {
 		return $this->name;
 	}
 
-	public function setName( $name ) {
-		$this->name = $name;
+	public function setName($name) {
+		$this->name = trim(strtolower(addslashes($name)));
 	}
 
 	public function getImgUrl() {
 		return $this->imgUrl;
 	}
 
-	public function setImgUrl( $imgUrl ) {
-		$this->imgUrl = $imgUrl;
+	public function setImgUrl($imgUrl) {
+
+		$this->imgUrl = trim($imgUrl);
 	}
+
+    public function getCreated(){
+        return $this->created_at;
+    }
+    public function setCreated(){
+        $this->created_at = date("Ymd");
+    }
+    public function getDeactivated(){
+        return $this->deactivated;
+    }
+    public function setDeactivated(){
+        $this->deactivated = date("Ymd");
+    }
 
 }
